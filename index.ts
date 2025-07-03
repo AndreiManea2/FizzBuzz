e870fb68fee83908b3da59ad1ba65d79d89ce499
AndreiManea2 <Andrei.Manea@softwire.com>
2025-07-03 12:30:21 +0300

    bang, bong, fezz, reverse logic done

--- index.ts
+++ index.ts
@@ -1,14 +1,51 @@
+function isMultipleOf(dividend: number, divisor: number): boolean {
+    return dividend % divisor === 0;
+}
+
 // This is our main function
 function fizzbuzz(): void {
-    for (let i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 != 0) {
-            console.log("Fizz");
-        } else if (i % 5 == 0 && i % 3 != 0) {
-            console.log("Buzz");
-        } else if (i % 3 == 0 && i % 5 == 0) {
-            console.log("FizzBuzz");
-        } else {
+    for (let i = 1; i <= 200; i++) {
+        // Variable to store the output for printing
+        let words: string[] = [];
+
+        if (isMultipleOf(i, 3)) {
+            words.push("Fizz");
+        }
+
+        if (isMultipleOf(i, 5)) {
+            words.push("Buzz");
+        }
+
+        if (isMultipleOf(i, 7)) {
+            words.push("Bang");
+        }
+
+        if (isMultipleOf(i, 13)) {
+            // Find index of first word starting with 'B'
+            const indexB = words.findIndex(word => word.startsWith('B'));
+            if (indexB === -1) {
+                // No word starting with B
+                words.push("Fezz");
+            } else {
+                // Insert Fezz before first word starting with B
+                words.splice(indexB, 0, "Fezz");
+            }
+        }
+
+        if (isMultipleOf(i, 11)) {
+            words = words.filter(word => word === "Fezz");
+            words.push("Bong");
+        }
+
+        if (isMultipleOf(i, 17)) {
+            words.reverse();
+        }
+
+        // If no words matched, print the number
+        if (words.length === 0) {
             console.log(i);
+        } else {
+            console.log(words.join(''));
         }
     }
 }
